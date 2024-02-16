5fd8320a5fbb096b5238a6577bc2d3952a5c1d3c
Simon M <simondotm@users.noreply.github.com>
2024-02-16 23:00:50 +0000

    Add emulator web target

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,25 @@
+#! node
 const esbuild = require('esbuild');
 const argv = require('minimist')(process.argv.slice(2));
 const { rimraf } = require('rimraf');
+const { copy } = require('esbuild-plugin-copy');
+const { join } = require('path');
 
 const minify = argv['minify'] ?? false;
 const watch = argv['watch'] ?? false;
 
-const config = {
+const sharedConfig = {
 	logLevel: 'info',
+	bundle: true,
+	format: 'cjs',
+	outdir: 'dist',
+	sourcemap: true,
+	minify,
+	external: ['vscode'], //Object.keys(require('../package.json').dependencies),
+};
+
+const extensionConfig = {
+	...sharedConfig,
 	entryPoints: [
 		{
 			out: 'main',
@@ -15,30 +28,57 @@ const config = {
 		{
 			out: 'server',
 			in: './src/server/server.ts'
-		}
+		},
 	],
-	bundle: true,
 	platform: 'node',
-	// outfile: 'build/main.js',
-	format: 'cjs',
-	outdir: 'dist',
-	sourcemap: true,
 	// target: 'node12',
-	minify,
-	external: ['vscode'] //Object.keys(require('../package.json').dependencies),
 };
 
+const webConfig = {
+	...sharedConfig,
+	entryPoints: [
+		{
+			out: 'emulator',
+			in: './src/emulator/index.ts'
+		},
+	],
+	platform: 'browser',
+	plugins: [
+		copyAssets('roms'),
+		copyAssets('sounds'),
+	],
+};
 
+function copyAssets(dir) {
+	const from = join('node_modules', 'jsbeeb', dir, '**', '*');
+	const to = join('dist', 'assets', 'jsbeeb', dir);
+	console.log(`copying assets from '${from}' to '${to}'`);
+	return 	copy({
+		// this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+		// if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+		resolveFrom: 'cwd',
+		assets: {
+			from: [from],
+			to: [to],
+		},
+		watch,
+		verbose: false,
+	});
+}
 
 
 
 async function main() {
-	await rimraf(config.outdir);
-	await esbuild.build(config);
+	await rimraf(sharedConfig.outdir);
+	await Promise.all([
+		esbuild.build(extensionConfig),
+		esbuild.build(webConfig),
+	]);
 	if (watch) {
 		console.log('watching...');
-		const ctx = await esbuild.context(config);
-		await ctx.watch();
+		const extensionContext = await esbuild.context(extensionConfig);
+		const webContext = await esbuild.context(webConfig);
+		await Promise.all([extensionContext.watch(), webContext.watch()]);
 	}
 }
 
