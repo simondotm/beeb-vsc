c52d148923b8246766b7aa4b9d2d81336c0387e0
Simon M <simondotm@users.noreply.github.com>
2024-03-22 09:12:07 +0000

    Add telemetry config for Azure application insights

---
+++ src/types/shared/telemetry-config.ts
@@ -0,0 +1,5 @@
+export const TELEMETRY_CONFIG = {
+  instrumentationKey: 'd3154e28-2627-411a-af7b-b2f2ec6ba00c',
+  connectionString:
+    'InstrumentationKey=d3154e28-2627-411a-af7b-b2f2ec6ba00c;IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/',
+}
