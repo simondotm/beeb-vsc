5af5d7b26ce694395ffd40c6181f83921f57fb7f
Tom Helm <tomhelm@gmail.com>
2024-04-19 13:31:11 +0100

    Pass test fixture path as workspace folder

--- src/client/test/runTest.ts
+++ src/client/test/runTest.ts
@@ -12,8 +12,17 @@ async function main() {
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './index')
 
+    const testWorkspace = path.resolve(
+      __dirname,
+      '../../../src/client/test/testFixture',
+    )
+
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath })
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [testWorkspace],
+    })
   } catch (err) {
     console.error('Failed to run tests')
     process.exit(1)
